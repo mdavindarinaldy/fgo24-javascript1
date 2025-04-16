b346b3a0a3406a3b204e05f58b17273479dfb97f
mdavindarinaldy <153896812+mdavindarinaldy@users.noreply.github.com>
2025-04-16 14:38:48 +0700

    Add files via upload

--- loop.js
+++ loop.js
@@ -1,9 +1,43 @@
+// FOR
+
+console.log("Perulangan For")
+
 let j = 5
 for (let i=1;i<=j;i++){
     if (i == 4) {
         console.log("fazztrack")
     } else {
         console.log(i)
+    }
+}
+
+for (let fi = 5; fi > 0; fi--) {
+    let temp = ""
+    for (let fj = 0; fj < fi; fj++) {
+        temp += "* "
+    }
+    console.log(temp)
+}
+
+for (let fx = 0; fx <= 5; fx++) {
+    let temp = ""
+    for (let fy = 0; fy < fx; fy++) {
+        temp += "* "
+    }
+    console.log(temp)
+}
+
+// WHILE
+
+console.log("Perulangan While")
+
+let wx = 0
+while (wx < 5) {
+    wx++
+    if (wx == 4) {
+        console.log("fazztrack")
+    } else {
+        console.log(wx)
     }
 }
 
@@ -20,7 +54,7 @@ while (x > 0) {
 }
 
 let k = 0
-while (k <= 5 ) {
+while (k <= 5) {
     let temp = ""
     let l = 0
     while (l < k) {
@@ -30,3 +64,43 @@ while (k <= 5 ) {
     console.log(temp)
     k++
 }
+
+// DO WHILE
+
+console.log("Perulangan Do While")
+
+let dx = 1
+do {
+    switch (dx) {
+        case 4:
+            console.log("fazztrack");
+            break;
+        default:
+            console.log(dx)    
+    }
+    dx++
+} while (dx <= 5)
+
+let di = 5
+do {
+    let temp = ""
+    let dj = 0
+    do {
+        temp += "* "
+        dj++
+    } while(dj < di)
+    di--
+    console.log(temp)
+} while(di > 0)
+
+let dk = 0
+do {
+    let temp = ""
+    let dl = 0
+    do {
+        temp += "* "
+        dl++
+    } while(dl < dk)
+    dk++
+    console.log(temp)
+} while(dk <= 5)
