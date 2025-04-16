89191358bfa53f246bd97429f816565cf10e3ba6
mdavindarinaldy <153896812+mdavindarinaldy@users.noreply.github.com>
2025-04-16 16:06:44 +0700

    Add files via upload

--- kotak.js
+++ kotak.js
@@ -1,9 +1,7 @@
 for (let i = 0; i<5; i++) {
     let temp = ""
     for (let j=0; j<5; j++) {
-        if (i==0 || i==4) {
-            temp += "* "
-        } else if (j==0 || j==4) {
+        if (i==0 || i==4 || j==0 || j==4) {
             temp += "* "
         } else {
             temp += "  "
