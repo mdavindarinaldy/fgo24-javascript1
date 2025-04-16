e57df7ad4d696783febaed86ab0b4dd890f3927f
mdavindarinaldy <153896812+mdavindarinaldy@users.noreply.github.com>
2025-04-16 10:20:13 +0700

    Add files via upload

---
+++ access-array.js
@@ -0,0 +1,9 @@
+// fgo24-javascript1
+
+const data = [["x", ["x","x","x",{
+    string: ["x", {
+        value: "hello"
+    }]
+}]]]
+
+console.log(data[0][1][3].string[1].value) // outputnya hello
