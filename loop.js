6e82a6137cf65c9ca5d29fd234d21384061692df
mdavindarinaldy <153896812+mdavindarinaldy@users.noreply.github.com>
2025-04-16 13:32:12 +0700

    Add files via upload

--- loop.js
+++ loop.js
@@ -1,8 +1,20 @@
-let x=5
-for (let i=1;i<=5;i++){
+let j = 5
+for (let i=1;i<=j;i++){
     if (i == 4) {
         console.log("fazztrack")
     } else {
         console.log(i)
     }
+}
+
+let x = 5
+while (x > 0) {
+    let temp = ""
+    let y = 0
+    while (y < x) {
+        temp += "* "
+        y++;
+    }
+    console.log(temp)
+    x--;
 }
